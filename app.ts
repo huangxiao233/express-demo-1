b1970e654550664928ea7159d41b2a53dcf66c41
黄笑 <huangxiao221@jd.com>
2021-03-01 14:28:27 +0800

    fuck

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ const { request } = require("express");
 import express from "express";
 const app = new (express as any)();
 const port = 2000;
-
+//fuck
 app.get("/index", function (request: any, response: any) {
   response.send("hell express!");
 });
